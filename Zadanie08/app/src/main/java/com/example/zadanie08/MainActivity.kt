64c93298f276e7605fce0b5d2481abc74d755e97
Krzysztof Gębka <krzysztof.gebka2001@gmail.com>
2022-12-31 16:41:54 +0100

    Exercise 08 - version 3.0(corrected)

--- Zadanie08/app/src/main/java/com/example/zadanie08/MainActivity.kt
+++ Zadanie08/app/src/main/java/com/example/zadanie08/MainActivity.kt
@@ -1,33 +1,64 @@
 package com.example.zadanie08
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.zadanie08.models.LoginRequest
+import com.example.zadanie08.models.LoginResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity()
 {
     lateinit var bLogin: Button
+    lateinit var etName: EditText
+    lateinit var etPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bLogin = findViewById(R.id.bLogin)
+        etName = findViewById(R.id.etName)
+        etPassword = findViewById(R.id.etPassword)
+
         bLogin.setOnClickListener{
-            val etLogin = findViewById<EditText>(R.id.etName)
-            val etPassword = findViewById<EditText>(R.id.etPassword)
-            val exampleLogin = "admin"
-            val examplePassword = "admin"
-            if (etLogin.text.toString()==exampleLogin && etPassword.text.toString()==examplePassword)
+            if (etName.text.toString().isEmpty() && etPassword.text.toString().isEmpty())
             {
-                Toast.makeText(this, "Udało się", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Nie wszystkie pola są uzupełnione", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
-            else
-            {
-                Toast.makeText(this, "Nie Udało się", Toast.LENGTH_LONG).show()
-            }
+            performLogin(etName.text.toString(), etPassword.text.toString())
         }
+    }
+
+    private fun performLogin(name: String, password: String)
+    {
+        val loginRequest = LoginRequest(name, password)
+        val call = RetrofitInstance.api.postLogin(loginRequest)
+
+        call.enqueue(object: Callback<LoginResponse>
+        {
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>)
+            {
+                val loginResponse = response.body()
+                if (loginResponse!!.result)
+                {
+                    Toast.makeText(this@MainActivity, "Logowanie się powiodło", Toast.LENGTH_SHORT).show()
+                }
+                else
+                {
+                    Toast.makeText(this@MainActivity, "Podanych danych logowania nie ma w bazie", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable)
+            {
+                Toast.makeText(this@MainActivity, "Blad laczenia z baza danych", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
